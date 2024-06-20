a0370a4423a050a5ae31fc0f8fbdc3880288aed1
Ukanwauzoma17 <lankerjunior13@gmail.com>
2024-06-20 10:57:20 +0100

    Admin model done

--- src/Admin/model/admin-model.ts
+++ src/Admin/model/admin-model.ts
@@ -1,44 +1,60 @@
 import {
-    BeforeSave,
-    Column,
-    IsEmail,
-    Model,
-    Table,
-    Unique,
-  } from "sequelize-typescript";
-  
-  @Table({ tableName: "Admin" })
-  class Admin extends Model {
-    @Column
-    declare firstName: string;
-  
-    @Column
-    declare lastName: string;
-  
-    @Unique
-    @IsEmail
-    @Column
-    declare email: string;
-  
-    @Column
-    declare password: string;
-  
-    @Column
-    declare phoneNumber: string;
-  
-    @Column
-    declare loginAttempts: number;
-  
-    @Column
-    declare locked: boolean;
+  BeforeSave,
+  Column,
+  IsEmail,
+  Model,
+  Table,
+  Unique,
+  Validate,
+} from "sequelize-typescript";
 
-    @BeforeSave
-    static async formatEmail(user: Admin) {
-      if (user.email) {
-        user.email = user.email.toLowerCase();
-      }
+@Table({ tableName: "Admin" })
+class Admin extends Model {
+  @Column
+  declare firstName: string;
+
+  @Column
+  declare lastName: string;
+
+  @Column
+  declare businessName: string;
+
+  @Unique
+  @IsEmail
+  @Column
+  declare email: string;
+
+  @Column
+  declare password: string;
+
+  @Column
+  declare phoneNumber: string;
+
+  @Column
+  declare loginAttempts: number;
+
+  @Column
+  declare locked: boolean;
+
+  @Column
+  declare newBusiness: boolean;
+
+  @Column
+  declare preExistingBusiness: boolean;
+
+  @Validate({
+    isIn: {
+      args: [["Nigeria"]], 
+      msg: "We're only avaailable in Nigeria for now",
+    },
+  })
+
+  @BeforeSave
+  static async formatEmail(user: Admin) {
+    if (user.email) {
+      user.email = user.email.toLowerCase();
     }
   }
-  
-  export default Admin;
-  
+}
+
+export default Admin;
---
+++ src/Admin/validator/sign-up-validator.ts
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+const userSchema = Joi.object({
+  firstName: Joi.string().trim().required(),
+  lastName: Joi.string().trim().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  confirmPassword: Joi.any().valid(Joi.ref("password")).required(),
+  phoneNumber: Joi.string().trim().min(11).max(11).required(),
+  businessName:Joi.string().trim().required,
+  country:Joi.string().required
+});
+
+export const validateUser = (user: any) => {
+  return userSchema.validate(user);
+};
